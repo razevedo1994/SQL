7b1255270e367e42979b28baa051c3462d7d9b5e
razevedo1994 <razevedo.contato@gmail.com>
2022-06-17 16:08:36 -0300

    create folder

---
+++ Udacity/fundamentos_de_analise_de_dados/limit.sql
@@ -0,0 +1,6 @@
+/* Escreva uma consulta que limita a resposta a apenas as 15 primeiras linhas e 
+inclua os campos occurred_at, account_id e channel na tabela web_events. */
+
+SELECT occurred_at, account_id, channel
+FROM web_events
+LIMIT 15;
